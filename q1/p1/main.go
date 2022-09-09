e84c626acb56c27ccfb301c2cd098e5d9ef682ca
jeongjd <110305802+jeongjd@users.noreply.github.com>
2022-09-08 22:40:25 -0400

    Update main.go

--- q1/p1/main.go
+++ q1/p1/main.go
@@ -2,30 +2,54 @@ package main
 
 import (
 	"fmt"
-	// import "time"
+	"time"
 )
 
+func p(x int) {
+	// Create new variables
+	count := 0
+	now := time.Now()
+
+	// Create and Initialize array
+	start := now
+	arr := []int{}
+	for i := 0; i < x+1; i++ {
+		arr = append(arr, i)
+		count++
+	}
+	fmt.Println(arr)
+	elapsed := time.Since(start)
+	fmt.Println(elapsed)
+
+	// Create and Initialize Slice
+	start = now
+	slice := make([]int, x+1, x+1)
+	for i := 0; i < x+1; i++ {
+		slice[i] = i
+		count++
+	}
+	fmt.Println(slice)
+	elapsed = time.Since(start)
+	fmt.Println(elapsed)
+
+	// Create and Initialize map
+	start = now
+	mymap := make(map[int]int)
+	for i := 0; i < x+1; i++ {
+		mymap[i] = i
+		count++
+	}
+	fmt.Println(mymap)
+	elapsed = time.Since(start)
+	fmt.Println(elapsed)
+}
+
 func main() {
-	fmt.Println("Enter an integer: ")
+	// Take user input
+	fmt.Printf("Enter an integer: ")
 	var x int
 	fmt.Scanln(&x)
 
-	arr := []int{}
-	var i int
-	for i = 0; i < x+1; i++ {
-		arr = append(arr, i)
-	}
-	fmt.Println(arr)
-
-	slice := make([]int, x+1, x+1)
-	for i = 0; i < x+1; i++ {
-		slice[i] = i
-	}
-	fmt.Println(slice)
-
-	var mymap = make(map[int]int)
-	for i = 0; i < x+1; i++ {
-		mymap[i] = i
-	}
-	fmt.Println(mymap)
+	// Call function p
+	p(x)
 }
